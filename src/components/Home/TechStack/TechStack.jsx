0accb58c210b620220fb4619c17190c3b9bada26
jcf-junior <h1ddenntc@gmail.com>
2025-07-02 19:59:41 +0100

    Prototyped tech stack marquee animations

--- src/components/Home/TechStack/TechStack.jsx
+++ src/components/Home/TechStack/TechStack.jsx
@@ -17,14 +17,14 @@ export default function TechStack() {
     return (
         <>
             <div className="techstack-container">
-                <Technology name="React" icon={techIcons.react}/>
-                <Technology name="Javascript" icon={techIcons.javascript}/>
-                <Technology name="Typescript" icon={techIcons.typescript}/>
-                <Technology name="HTML" icon={techIcons.html}/>
-                <Technology name="CSS" icon={techIcons.css}/>
-                <Technology name="Tailwind" icon={techIcons.tailwind}/>
-                <Technology name="Express.JS" icon={techIcons.express}/>
-                <Technology name="Next.JS" icon={techIcons.next}/>
+                <Technology name="React" id={1} icon={techIcons.react}/>
+                <Technology name="Javascript" id={2} icon={techIcons.javascript}/>
+                <Technology name="Typescript" id={3} icon={techIcons.typescript}/>
+                <Technology name="HTML" id={4} icon={techIcons.html}/>
+                <Technology name="CSS" id={5} icon={techIcons.css}/>
+                <Technology name="Tailwind" id={6} icon={techIcons.tailwind}/>
+                <Technology name="Express.JS" id={7} icon={techIcons.express}/>
+                <Technology name="Next.JS" id={8} icon={techIcons.next}/>
             </div>
         </>
     )
--- src/components/Home/TechStack/Technology.jsx
+++ src/components/Home/TechStack/Technology.jsx
@@ -1,11 +1,11 @@
 
-export default function Technology({name, icon, ...rest}) {
+export default function Technology({name, icon, id, ...rest}) {
 
     return (
         <>
-            <div className="technology-square">
+            <div className={`technology-square square-${id}`}>
                 <img className="technology-img" src={icon} alt={`logo of ${name}`} />
-                <p>{name}</p>
+                {/* <p>{name}</p> */}
 
             </div>
         </>
