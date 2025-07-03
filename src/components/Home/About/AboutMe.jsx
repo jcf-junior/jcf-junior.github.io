e3e7a363f9b78206f5c19ceb41162ecae18176dd
jcf-junior <h1ddenntc@gmail.com>
2025-07-03 06:29:02 +0100

    Clean up

--- src/components/Home/About/AboutMe.jsx
+++ src/components/Home/About/AboutMe.jsx
@@ -13,8 +13,8 @@ export default function AboutMe() {
                     <span className="about-h1">DEVELOPER</span>
                 </h1>
                 <p className="description">Highly energetic {age}-year-old developer driven by curiosity and caffeine. 
-                <br/>
-                My experience includes testing enterprise-level apps as a freelancer and building web apps from scratch one coffee at a time. 
+                
+                My experience includes testing enterprise-level apps as a freelancer and building web apps from scratch. 
                 </p>
             </div>
             
